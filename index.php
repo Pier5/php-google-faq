1a15a511c0888e65a85cab09e9e3de4d145be75f
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-11 15:48:48 +0200

    -inizio pagina intera- /aggiunta stile

--- index.php
+++ index.php
@@ -60,17 +60,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- link fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- link css -->
+    <link rel="stylesheet" href="style.css">
+
     <title>PHP Gogle FAQ</title>
 </head>
 <body>
 
+    <div class="container">
     <?php
         foreach($arrQA as $element) { ?>
             <?php echo $element['question'] ?>
             <?php echo $element['answer'] ?>
         <?php
         } ?>
+    </div>
 
 </body>
 </html>
